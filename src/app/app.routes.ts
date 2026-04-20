fc948de47e21dfa00500aa5ea9800ae497e973df
BBevilacqua <bericobski@outlook.com.ar>
2026-04-20 19:01:01 -0300

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AddGameForm } from './components/add-game-form/add-game-form';
+import { Login } from './components/login/login';
+import { Register } from './components/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'add-game-form', component: AddGameForm },
+    { path: 'login', component: Login },
+    { path: 'register', component: Register },
+    { path: '', redirectTo: 'login', pathMatch: 'full' }
+
+];
---
+++ src/app/components/add-game-form/add-game-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-game-form',
+  imports: [],
+  templateUrl: './add-game-form.html',
+  styleUrl: './add-game-form.scss',
+})
+export class AddGameForm {}
